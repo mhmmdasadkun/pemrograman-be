88fffe612ddc250661e410b480f36d5204ba8dc2
mhmmdasadkun <mhmmdasadkun@gmail.com>
2023-10-28 09:30:46 +0700

    Pertemuan 5 - Restful API, Migrate, Model & Databases

---
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::all();
+        $data = [
+            'message' => 'Menampilkan semua murid',
+            'data' => $students
+        ];
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = [
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan
+        ];
+
+        $student = Student::create($input);
+
+        $data = [
+            'message' => 'Murid berhasil ditambahkan!',
+            'data' => $student
+        ];
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $student = Student::find($id);
+        if (!$student) {
+            return response()->json(['message' => 'Murid tidak ditemukan!'], 404);
+        }
+
+        $data = [
+            'message' => 'Menampilkan detail murid',
+            'data' => $student
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $student = Student::find($id);
+        if (!$student) {
+            return response()->json(['message' => 'Murid tidak ditemukan!'], 404);
+        }
+
+        $input = [
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan
+        ];
+
+        $student->update($input);
+
+        $data = [
+            'message' => 'Murid berhasil diubah!',
+            'data' => $student
+        ];
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $student = Student::find($id);
+        if (!$student) {
+            return response()->json(['message' => 'Murid tidak ditemukan!'], 404);
+        }
+        $student->delete();
+
+        $students = Student::all();
+
+        $data = [
+            'message' => 'Murid berhasil dihapus!',
+            'data' => $students
+        ];
+        return response()->json($data, 200);
+    }
+}
--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,10 @@ Route::get('/animals', [AnimalController::class, 'index']);
 Route::post('/animals', [AnimalController::class, 'store']);
 Route::put('/animals/{index}', [AnimalController::class, 'update']);
 Route::delete('/animals/{index}', [AnimalController::class, 'destroy']);
+
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'show']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+
